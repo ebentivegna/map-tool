9e68bf892f3309fb4c3db3c5a4e0b464a018fa0e
ebentive <ebentive@mocha.(none)>
2015-07-08 20:14:17 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Django Application
+## My MAP violation tool
+
+Django 1.8.2
+Python 3.4
---
+++ map_tool/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Ksag(models.model):
+
+    pass
+
+class Sku(models.model):
+
+    pass
+
+
